523a9c149f05c807f9f5b45c79e9359b63b9a628
ahmadzainulmufid <ahmadzainulmufid27@gmail.com>
2025-04-02 08:32:22 +0700

    update code

--- app/src/main/java/com/example/midwiferyapplication/ui/visimisi/VisiActivity.kt
+++ app/src/main/java/com/example/midwiferyapplication/ui/visimisi/VisiActivity.kt
@@ -1,6 +1,7 @@
 package com.example.midwiferyapplication.ui.visimisi
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +12,10 @@ class VisiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visi)
+        val btnBack: ImageButton = findViewById(R.id.btn_back)
+        btnBack.setOnClickListener {
+            // Handle the back button press
+            onBackPressed() // This will navigate back to the previous activity
+        }
     }
 }
